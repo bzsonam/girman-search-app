da4440f53feec167299c6f4c8b866a7fc1400fd9
Sonam Bazzad <sssbazzad@gmail.com>
2025-01-26 21:50:58 +0530

    Mobile Responsiveness Added

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,7 @@
-// "use client";
 import Navbar from "../components/Navbar";
-import SearchBox from "../components/SearchBox";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import SearchBox from "../screens/HomePage";
 
 export default function Home() {
-  console.log("fetch");
-
   return (
     <div>
       <Navbar />
